b856c3361cb7cf0ce0f5be039b7ba65639499615
Luiza Bretas <80011623+ArthLoki@users.noreply.github.com>
2024-04-24 18:02:27 -0300

    bugfix test_generator

--- MoireApi/flaskapp.py
+++ MoireApi/flaskapp.py
@@ -1,21 +1,18 @@
 from flask import Flask, render_template, request, jsonify
-from flask import flash, redirect, url_for, send_from_directory
+# from flask import flash, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
-from moire.test_04 import load_and_evaluate
+from moire.test_moire_api import load_and_evaluate
 from moire.createTrainingData import augmentAndTrasformImage
 
 import os
+from datetime import datetime
 
 UPLOAD_FOLDER = 'uploads/images'
 
-# UPLOAD_FOLDER_TRAIN = 'uploads/images/train'
-# UPLOAD_FOLDER_TEST = 'uploads/images/test'
-
-UPLOAD_FOLDER_TRAIN = str(os.path.join(UPLOAD_FOLDER, 'train')).replace('/', '\\')
-
-UPLOAD_FOLDER_TEST = str(os.path.join(UPLOAD_FOLDER, 'test')).replace('/', '\\')
-UPLOAD_FOLDER_TEST_PROCESSED = str(os.path.join(UPLOAD_FOLDER, 'test_processed')).replace('/', '\\')
+UPLOAD_FOLDER_TRAIN = str(os.path.join(UPLOAD_FOLDER, 'train')).replace('\\', '/')
+UPLOAD_FOLDER_TEST = str(os.path.join(UPLOAD_FOLDER, 'test')).replace('\\', '/')
+UPLOAD_FOLDER_TEST_PROCESSED = str(os.path.join(UPLOAD_FOLDER, 'test_processed')).replace('\\', '/')
 
 MODEL_FOLDER = 'uploads/savedmodels'
 
@@ -29,7 +26,7 @@ app.config['UPLOAD_FOLDER_TRAIN'] = UPLOAD_FOLDER_TRAIN
 app.config['MODEL_FOLDER'] = MODEL_FOLDER
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['FLASK_ENV'] = 'development'  # Set Flask environment to development
-app.debug = True  # Enable Flask debug mode
+app.debug = app.config['FLASK_ENV'] == 'development' # Enable Flask debug mode
 # app.add_url_rule("/uploads/<name>", endpoint="download_file", build_only=True)
 
 ## Create folders if they don't exist
@@ -59,87 +56,88 @@ def allowed_file(filename, filetype):
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            # flash('No file part')
-            # return redirect(request.url)
-            return jsonify({'error': 'No file part'})
-
-        file = request.files['file']
-
         # CHECK MODEL FILE
-
+        # If the model folder is empty, the user needs to upload a model. If there's a model in the folder,
+        # the upload button is disabled and there's no need to upload it again
+        # If the model upload is needed, all the uploads (model + image) will take more time to finish
         if len(os.listdir(app.config['MODEL_FOLDER'])) == 0:
             # check if the post request has the file part
             saved_model = request.files['savedmodel']
             if saved_model.filename == '':
-                return jsonify({'error': 'No selected model'})
+                return jsonify({'Error': 'No selected model'})
 
             if saved_model and allowed_file(saved_model.filename, 1):
-                modelname = secure_filename(str(saved_model.filename))
-                if not os.path.exists(os.path.join(app.config['MODEL_FOLDER'], modelname)):
-                    saved_model.save(os.path.join(app.config['MODEL_FOLDER'], modelname))
+                clear_model_folder()
+                model_name = secure_filename(str(saved_model.filename))
+                if not os.path.exists(os.path.join(app.config['MODEL_FOLDER'], model_name)):
+                    saved_model.save(os.path.join(app.config['MODEL_FOLDER'], model_name))
             else:
-                return jsonify({'error': 'Model file not allowed'})
+                return jsonify({'Error': 'Model file not allowed'})
         else:
-            # saved_model = None
-            modelname = os.listdir(app.config['MODEL_FOLDER'])[0] if os.listdir(app.config['MODEL_FOLDER']) else None
-            # print(modelname)
+            model_name = os.listdir(app.config['MODEL_FOLDER'])[0] if os.listdir(app.config['MODEL_FOLDER']) else None
 
         # CHECK IMAGE FILE
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            # flash('No file part')
+            # return redirect(request.url)
+            return jsonify({'Error': 'No file part'})
 
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
+        file = request.files['file']
+
         if file.filename == '':
             # flash('No selected file')
             # return redirect(request.url)
             return jsonify({'error': 'No selected file'})
 
-        if file and allowed_file(file.filename, 0) and modelname is not None:
+        if file and allowed_file(file.filename, 0) and model_name is not None:
             filename = secure_filename(str(file.filename))
-            # modelname = os.listdir(app.config['UPLOAD_FOLDER_TEST'])[0]
+            # model_name = os.listdir(app.config['UPLOAD_FOLDER_TEST'])[0]
 
             file.save(os.path.join(app.config['UPLOAD_FOLDER_TEST'], filename))
             # return redirect(url_for('download_file', name=filename))
 
             # CHAMADA MOIRE
-            modelpath = str(os.path.join(app.config['MODEL_FOLDER'], modelname)).replace('/', '\\')
+            model_path = str(os.path.join(app.config['MODEL_FOLDER'], model_name)).replace('\\', '/')
 
             if len(os.listdir(app.config['UPLOAD_FOLDER_TEST'])) > 0:
                 imageTransformed = augmentAndTrasformImage(filename,
                                                             app.config['UPLOAD_FOLDER_TEST'],
                                                             app.config['UPLOAD_FOLDER_TEST_PROCESSED'])
 
-                # print(os.listdir(app.config['UPLOAD_FOLDER_TEST']))
-                # print(os.listdir(app.config['UPLOAD_FOLDER_TEST_PROCESSED']))
-
                 if imageTransformed and len(os.listdir(app.config['UPLOAD_FOLDER_TEST_PROCESSED'])) > 0:
-                    status = load_and_evaluate(modelpath,
-                                                app.config['UPLOAD_FOLDER_TEST'],
-                                                app.config['UPLOAD_FOLDER_TRAIN'],
-                                                True)
-                    clear_folders()
+                    status = load_and_evaluate(model_path,
+                                                app.config['UPLOAD_FOLDER_TEST_PROCESSED'])
+
+                    clear_test_folders()
+
+                    if status.get('Error') is not None:
+                        clear_test_folders()
+                        return jsonify(status)
                 else:
-                    clear_folders()
-                    return jsonify({'error': 'image augment and transformation failed or folder test_processed is empty'})
+                    clear_test_folders()
+                    return jsonify({'Error': 'image augment and transformation failed or folder test_processed is empty'})
 
-                return jsonify({'filename': filename, 'status': status, 'modelname': modelname})
-            return jsonify({'error': 'No file found'})
+                return jsonify({'filename': filename,
+                                'moire': status.get('moire'),
+                                'model_name': model_name,
+                                'datetime': datetime.now()})
 
-            # return render_template(
-            #     "response.html",
-            #     filename=filename.upper(),
-            #     loss=status.get('loss'),
-            #     accuracy=status.get('accuracy'),
-            #     cm=status.get('confusion_matrix'),
-            #     cr=status.get('classification_report'),
-            #     # file=url_for('download_file', name=filename)
-            # )
+                # return render_template(
+                #     "response.html",
+                #     filename=filename.upper(),
+                #     loss=status.get('loss'),
+                #     accuracy=status.get('accuracy'),
+                #     cm=status.get('confusion_matrix'),
+                #     cr=status.get('classification_report'),
+                #     file=url_for('download_file', name=filename)
+                # )
+            return jsonify({'Error': 'No file found'})
 
     return render_template('index.html', empty_model_folder=len(os.listdir(app.config['UPLOAD_FOLDER_TEST'])) == 0)
 
 
-def clear_folders():
+def clear_test_folders():
     if len(os.listdir(app.config['UPLOAD_FOLDER_TEST'])) > 0:
         for filename_image in os.listdir(app.config['UPLOAD_FOLDER_TEST']):
             os.remove(os.path.join(app.config['UPLOAD_FOLDER_TEST'], filename_image))
@@ -147,6 +145,12 @@ def clear_folders():
     if len(os.listdir(app.config['UPLOAD_FOLDER_TEST_PROCESSED'])) > 0:
         for filename_tiff in os.listdir(app.config['UPLOAD_FOLDER_TEST_PROCESSED']):
             os.remove(os.path.join(app.config['UPLOAD_FOLDER_TEST_PROCESSED'], filename_tiff))
+    return
+
+def clear_model_folder():
+    if len(os.listdir(app.config['MODEL_FOLDER'])) > 0:
+        for model_name in os.listdir(app.config['MODEL_FOLDER']):
+            os.remove(os.path.join(app.config['MODEL_FOLDER'], model_name))
     return
 
 
---
+++ MoireApi/moire/test_moire_api.py
@@ -0,0 +1,64 @@
+import argparse
+import numpy as np
+from tensorflow.keras.models import load_model
+import logging
+import os
+from PIL import Image
+
+WIDTH = 750
+HEIGHT = 1000
+TRANSFORM_COUNT = 12  # Número de transformações por imagem original
+
+
+def load_and_evaluate(model_path, test_dir):
+    logging.basicConfig(level=logging.INFO)
+
+    # Load the trained model
+    try:
+        print("Loading model...")
+        model = load_model(model_path)
+        print("Model loaded successfully!")
+    except Exception as e:
+        return {'Error': f'Error loading {model_path.split("/")[-1]} in test_moire_api.py:\n{e}'}
+
+    results = []
+    for i in range(TRANSFORM_COUNT):
+        img_path = os.path.join(test_dir, os.listdir(test_dir)[i])
+
+        # Preprocessing image
+        try:
+            print(f"Preprocessing image {i+1}...")
+            test_generator = Image.open(img_path).convert('L')  # Convert to grayscale
+            test_generator = test_generator.resize((WIDTH, HEIGHT))
+            test_generator = np.expand_dims(np.array(test_generator), axis=0)
+            test_generator = np.expand_dims(test_generator, axis=-1)  # Add a singleton dimension for channels
+            test_generator = test_generator.astype('float32') / 255.0  # Normalize pixel values
+
+            print(f"Preprocess image {i+1} successfully!")
+        except Exception as e:
+            return {'Error': f'Error preprocessing {os.listdir(test_dir)[i]} in test_moire_api.py:\n{e}'}
+
+        # Previsões do conjunto de teste
+        try:
+            logging.info("Gerando previsões para o conjunto de teste...")
+            predictions = model.predict(test_generator)
+        except Exception as e:
+            return {
+                'Error': f'Error generating predictions for {model_path.split("/")[-1]} in test_moire_api.py: {e}'}
+
+        results.append(True if predictions[0][0] > 0.5 else False)
+
+    count_moire = results.count(True)
+    if count_moire < len(results)//2:
+        is_moire = False
+    else:
+        is_moire = True
+
+    return {
+        # "loss": results[0],
+        # "accuracy": results[1],
+        # 'confusion_matrix': cm,
+        # 'classification_report': class_report
+        # 'prediction': predictions[0],
+        'moire': is_moire
+    }
